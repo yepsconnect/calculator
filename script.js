16f1575f55ee9547a5ef668ba028f315af29d5b1
Dmitry Kungurov <125030370+yepsconnect@users.noreply.github.com>
2013-03-22 20:36:12 +0400

    update fuctions

--- script.js
+++ script.js
@@ -1,74 +1,24 @@
+var result = document.getElementById("result");
 var number = null;
 var operator = null;
 
-var btn0 = document.getElementById("btn-0");
-var btn1 = document.getElementById("btn-1");
-var btn2 = document.getElementById("btn-2");
-var btn3 = document.getElementById("btn-3");
-var btn4 = document.getElementById("btn-4");
-var btn5 = document.getElementById("btn-5");
-var btn6 = document.getElementById("btn-6");
-var btn7 = document.getElementById("btn-7");
-var btn8 = document.getElementById("btn-8");
-var btn9 = document.getElementById("btn-9");
+function handleNumberClick(e) {
+  var digit = e.target.dataset.value;
+  if (result.innerText === "Error") {
+    result.innerText = digit;
+  } else {
+    result.innerText += digit;
+  }
+}
 
-var btnDecimal = document.getElementById("btn-decimal");
-var btnEquals = document.getElementById("btn-equals");
-var btnClear = document.getElementById("btn-clear");
+function handleDecimalClick() {
+  if (!result.innerText.includes(".")) {
+    result.innerText += ".";
+  }
+}
 
-var result = document.getElementById("result");
-
-btn0.addEventListener("click", function () {
-  result.innerText += "0";
-});
-
-btn1.addEventListener("click", function () {
-  result.innerText += "1";
-});
-
-btn2.addEventListener("click", function () {
-  result.innerText += "2";
-});
-
-btn3.addEventListener("click", function () {
-  result.innerText += "3";
-});
-
-btn4.addEventListener("click", function () {
-  result.innerText += "4";
-});
-
-btn5.addEventListener("click", function () {
-  result.innerText += "5";
-});
-
-btn6.addEventListener("click", function () {
-  result.innerText += "6";
-});
-
-btn7.addEventListener("click", function () {
-  result.innerText += "7";
-});
-
-btn8.addEventListener("click", function () {
-  result.innerText += "8";
-});
-
-btn9.addEventListener("click", function () {
-  result.innerText += "9";
-});
-
-btnDecimal.addEventListener("click", function () {
-  result.innerText += ".";
-});
-
-btnClear.addEventListener("click", function () {
-  result.innerText = "";
-  number = null;
-  operator = null;
-});
-
-function useOperation(op) {
+function handleOperatorClick(e) {
+  var op = e.target.dataset.value;
   if (number !== null && operator !== null) {
     equals();
   } else {
@@ -87,9 +37,37 @@ function equals() {
   } else if (operator === "*") {
     result.innerText = number * parseFloat(result.innerText);
   } else if (operator === "/") {
-    result.innerText = number / parseFloat(result.innerText);
+    var divisor = parseFloat(result.innerText);
+    if (divisor !== 0) {
+      result.innerText = number / divisor;
+    } else {
+      result.innerText = "Error";
+    }
   }
 
   number = parseFloat(result.innerText);
   operator = null;
 }
+
+function clear() {
+  result.innerText = "";
+  number = null;
+  operator = null;
+}
+
+var numberButtons = document.querySelectorAll(".number");
+for (var i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click", handleNumberClick);
+}
+
+document
+  .querySelector(".decimal")
+  .addEventListener("click", handleDecimalClick);
+
+var operatorButtons = document.querySelectorAll(".operator");
+for (var i = 0; i < operatorButtons.length; i++) {
+  operatorButtons[i].addEventListener("click", handleOperatorClick);
+}
+
+document.getElementById("btn-equals").addEventListener("click", equals);
+document.getElementById("btn-clear").addEventListener("click", clear);
